f855ff931a31af184e3bb0f0338b95bf2e3f9ec5
oscarbaruffa <16580713+oscarbaruffa@users.noreply.github.com>
2021-12-22 20:13:45 +0100

    trying ot make a markdown file

--- book_content.R
+++ book_content.R
@@ -4,6 +4,8 @@
 library(dplyr)
 library(googlesheets4)
 library(glue)
+library(rmarkdown)
+library(readr)
 
 
 
@@ -25,9 +27,41 @@ books_source <- read_sheet("https://docs.google.com/spreadsheets/d/1vufdtrIzF5wb
 # Create book content -----------------------------------------------------
 
 
-  for (entry in 1:nrow(books_source)) {
-    
-    chapter
-    
-    glue("The chapter is {chapter}, the book is {title")
-  }
+ prep_booksource <- books_source %>% 
+  # mutate(content_markdown = glue('## {title}  [{author1}]({bio1}) [{author2}]({bio2}) [{author3}]({bio3}) {others}  {description}  {paid} {amount_usd}  {link}',
+  #                                .na = ""))
+
+mutate(content_markdown = glue("## {title}",
+                               "[{author1}]({bio1})",
+                               "[{author2}]({bio2})",
+                               "[{author3}]({bio3})",
+                               "{others} ",
+                               "{description}",
+                               "{paid} {amount_usd}",
+                               "{link}",
+                               .na = "", 
+                               sep = "  "))
+
+chapters <- books_source %>% 
+  select(chapters) %>% 
+  distinct(chapters) %>% 
+  pull() 
+
+
+markdown_content <-  ""
+
+ for (chapter in chapters) {
+   
+   
+   
+   chapter_content <- prep_booksource %>% 
+     filter(chapters == chapter) %>% 
+     select(content_markdown)
+   
+   markdown_content <- paste(markdown_content, "#", chapter,chapter_content, sep = "  ")
+   
+   
+ }
+
+write.table(markdown_content)
+
